550b7d53018cc914b03c4c94a68019939b7c2654
ClaudiaCalero <claudiaclrduro@gmail.com>
2023-11-01 13:25:20 +0100

    Question class

--- game/src/main/java/com/ClaudiaCalero/game/Question.java
+++ game/src/main/java/com/ClaudiaCalero/game/Question.java
@@ -1,4 +1,48 @@
 package com.ClaudiaCalero.game;
 
-public class Question {
+public abstract class Question {
+    private String questionText; // La pregunta en sí
+    private String correctAnswer; // La respuesta correcta
+    private String hint; // La pista que puede ayudar al jugador
+
+    // Constructor
+    public Question(String questionText, String correctAnswer, String hint) {
+        this.questionText = questionText;
+        this.correctAnswer = correctAnswer;
+        this.hint = hint;
+    }
+
+    // Métodos para obtener el texto de la pregunta
+    public String getQuestionText() {
+        return questionText;
+    }
+
+    // Métodos para establecer el texto de la pregunta
+    public void setQuestionText(String questionText) {
+        this.questionText = questionText;
+    }
+
+    // Métodos para obtener la respuesta correcta
+    public String getCorrectAnswer() {
+        return correctAnswer;
+    }
+
+    // Métodos para establecer la respuesta correcta
+    public void setCorrectAnswer(String correctAnswer) {
+        this.correctAnswer = correctAnswer;
+    }
+
+    // Métodos para obtener la pista
+    public String getHint() {
+        return hint;
+    }
+
+    // Métodos para establecer la pista
+    public void setHint(String hint) {
+        this.hint = hint;
+    }
+
+    // Métodos abstractos para mostrar la pregunta y verificar la respuesta
+    public abstract void displayQuestion();
+    public abstract boolean isCorrect(String userAnswer);
 }
