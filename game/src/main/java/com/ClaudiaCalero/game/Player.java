f4d83363f0e40c5a948f0189144f1d0e48afa0a6
ClaudiaCalero <claudiaclrduro@gmail.com>
2023-11-01 13:28:23 +0100

    Player class

--- game/src/main/java/com/ClaudiaCalero/game/Player.java
+++ game/src/main/java/com/ClaudiaCalero/game/Player.java
@@ -1,4 +1,31 @@
 package com.ClaudiaCalero.game;
 
 public class Player {
+    private String name;
+    private int score;
+
+    public Player(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public void increaseScore(){
+        score++;
+    }
 }
